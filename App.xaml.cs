17d59e38911ea1282f959d4acb0da9b4046fbb9c
monirol4664-glitch <monirol4664@gmail.com>
2026-04-18 20:37:29 +0600

    Refactor MainPage initialization logic
    Updated MainPage initialization to handle obsolete cases.

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,6 +7,20 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
-        MainPage = new MainPage();
+        
+        // Fix for obsolete MainPage
+        if (Windows.Count > 0)
+        {
+            Windows[0].Page = new MainPage();
+        }
+        else
+        {
+            MainPage = new MainPage();
+        }
+    }
+    
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new MainPage());
     }
 }
