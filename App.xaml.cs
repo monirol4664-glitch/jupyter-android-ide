61f790a10fe5c78569462ea82e31766e26733d4e
monirol4664-glitch <monirol4664@gmail.com>
2026-04-18 20:43:32 +0600

    Simplify MainPage initialization in App.xaml.cs
    Removed obsolete MainPage handling and simplified initialization.

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,20 +7,6 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
-        
-        // Fix for obsolete MainPage
-        if (Windows.Count > 0)
-        {
-            Windows[0].Page = new MainPage();
-        }
-        else
-        {
-            MainPage = new MainPage();
-        }
-    }
-    
-    protected override Window CreateWindow(IActivationState? activationState)
-    {
-        return new Window(new MainPage());
+        MainPage = new MainPage();
     }
 }
